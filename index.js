a7296933007a6773a729d91503c19c21777bc8d1
michael haberman <you@example.com>
2018-12-17 16:07:18 +0200

    Adding p3p support

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const cookieParser = require('cookie-parser')
 
 
 
+app.use((req, res, next) => {
+    console.log('start p3p');
+
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("P3P", 'CP=\"ALL IND DSP COR ADM CONo CUR CUSo IVAo IVDo PSA PSD TAI TELo OUR SAMo CNT COM INT NAV ONL PHY PRE PUR UNI\"');
+    next();
+})
 app.use(cors({ origin: true, credentials: true }));
+app.use((req, res, next) => {
+    console.log('after cors')
+    next();
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(allowAll);
