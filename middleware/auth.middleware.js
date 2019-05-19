44a45e15d89f06acbb52650b66d7be4c4a4553bc
michael haberman <you@example.com>
2019-05-19 20:03:07 +0300

    fix

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -3,7 +3,7 @@ const userService = require("../services/user.service");
 
 function auth(req,res,next) {
     console.log('in auth middleware')
-    const imcrToken = req.cookies['imcr-token'] || req.query.token;
+    const imcrToken = req.query.token;
 
     if(imcrToken && userService.checkToken(imcrToken)) {
         next();
