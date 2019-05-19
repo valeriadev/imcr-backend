423688e3cb9f4108acbd821471bb6d086ce30d38
michael haberman <you@example.com>
2019-05-19 19:12:01 +0300

    fix cors

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const cookieParser = require('cookie-parser')
 
 
 app.use((req, res, next) => {
-    // res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Credentials", "true");
     // res.header("P3P", 'CP=\"ALL IND DSP COR ADM CONo CUR CUSo IVAo IVDo PSA PSD TAI TELo OUR SAMo CNT COM INT NAV ONL PHY PRE PUR UNI\"');
     next();
 })
-app.use(cors({ credentials: true, allowedHeaders:['Content-Type','Origin'], origin:['http://localhost:3000','http://imcr.info']}));
+app.use(cors({ credentials: true, allowedHeaders:['Content-Type','Origin', "Accept"], origin:['http://localhost:3000','http://imcr.info']}));
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -30,7 +30,7 @@ app.listen(8080, (err) => {
         console.error("server can't listen to port 8080", err);
         throw err;
     } else {
-        console.log("server is alive");
+        console.log("server is alive - 8080");
     }
 });
 
